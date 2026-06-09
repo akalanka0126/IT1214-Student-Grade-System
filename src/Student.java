a78a85ddc4468e704a9f334bac47ce1be4d8565f
Akalanka <2022ict134@stu.vau.ac.lk>
2026-06-09 22:48:08 +0530

    Created a Student class

---
+++ src/Student.java
@@ -0,0 +1,27 @@
+public class Student {
+    private String id;
+    private String name;
+    private double marks;
+
+    public Student(String id, String name, double marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+
+    public void displayStudent() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Marks: " + marks);
+    }
+}
